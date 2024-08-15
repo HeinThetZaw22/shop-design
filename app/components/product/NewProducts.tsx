3908ec597e725a92124554d58f91c65f81383480
HeinThetZaw22 <heinthetzaw2212@gmail.com>
2024-08-15 09:24:21 +0630

    padding changed

--- app/components/product/NewProducts.tsx
+++ app/components/product/NewProducts.tsx
@@ -57,7 +57,7 @@ const NewProducts = () => {
         className=" 
              pt-10 grid 
              scroll-smooth
-             grid-cols-2
+             grid-cols-1
              sm:grid-cols-2 
              lg:grid-cols-4 
              gap-5
